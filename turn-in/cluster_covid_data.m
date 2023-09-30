3f23534a92a131e6aa89e96d8d3aab0e07f1c2e3
ehitale <elatihe@gmail.com>
2023-09-30 17:41:35 -0500

    initial clustering

--- turn-in/cluster_covid_data.m
+++ turn-in/cluster_covid_data.m
@@ -1,4 +1,16 @@
 load ("../COVIDbyCounty.mat")
 
-[idx, C] = kmeans(CNTY_COVID, 9);
-plot(C)
+k = 9;
+[idx, C] = kmeans(CNTY_COVID', k);
+
+
+for i = 1:9
+    figure;
+    caseTrajectories = CNTY_COVID(idx == i, :)';
+    plot(caseTrajectories);
+    xlabel("week");
+    ylabel("cases per 100k");
+end
+
+
+
