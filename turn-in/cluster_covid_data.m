0eba63cd0484e9ee0818b3a7e0cc54633eb09fe0
ehitale <elatihe@gmail.com>
2023-09-30 18:52:58 -0500

    added centroid and graph info.

--- turn-in/cluster_covid_data.m
+++ turn-in/cluster_covid_data.m
@@ -1,13 +1,23 @@
 load ("../COVIDbyCounty.mat")
 
-k = 9;
-[idx, C] = kmeans(CNTY_COVID', k);
-
+k = 9; % for 9 divisions
+[idx, C] = kmeans(CNTY_COVID, k); % this finds the kmeans of a transposed CNTY_COVID. I needed to transpose CNTY_COVID because the variables in CNTY_COVID are rows, whereas kmeans considers columns to be variables.
 
 for i = 1:9
+    caseTrajectories = CNTY_COVID(idx == i, :)'; % uses logical indexing on CNTY_COVID transposed to find the divisions.
+    
     figure;
-    caseTrajectories = CNTY_COVID(idx == i, :)';
+    tiledlayout(2, 1)
+
+    nexttile;
+    plot(C(i, :));
+    title(['Centroid ', num2str(i)]);
+    xlabel("week");
+    ylabel("cases per 100k");
+
+    nexttile;
     plot(caseTrajectories);
+    title(['Cluster ', num2str(i)]);
     xlabel("week");
     ylabel("cases per 100k");
 end
