d2406d78e623e6871139a3bedf5eaf21a6c2c54c
ehitale <elatihe@gmail.com>
2023-09-30 20:21:48 -0500

    cleaned up 'classify', modified 'cluster', added .mat files to '.gitignore'

--- turn-in/classify_covid_data.m
+++ turn-in/classify_covid_data.m
@@ -1,3 +1,6 @@
+load ("../COVIDbyCounty.mat")
+load ("cluster_covid_data.mat")
+
 for idx_1 = 1:9
     figure; 
     x = CNTY_COVID(idx == idx_1,:)';
--- turn-in/cluster_covid_data.m
+++ turn-in/cluster_covid_data.m
@@ -1,10 +1,11 @@
 load ("../COVIDbyCounty.mat")
+save ("cluster_covid_data.mat")
 
 k = 9; % for 9 divisions
-[idx, C] = kmeans(CNTY_COVID, k); % this finds the kmeans of a transposed CNTY_COVID. I needed to transpose CNTY_COVID because the variables in CNTY_COVID are rows, whereas kmeans considers columns to be variables.
+[idx, C] = kmeans(CNTY_COVID, k, "Replicates", 5); % this finds the kmeans  CNTY_COVID. I needed to transpose CNTY_COVID because the variables in CNTY_COVID are rows, whereas kmeans considers columns to be variables. Replication makes the kmeans look a whole lot better. 
 
 for i = 1:9
-    caseTrajectories = CNTY_COVID(idx == i, :)'; % uses logical indexing on CNTY_COVID transposed to find the divisions.
+    caseTrajectories = CNTY_COVID(idx == i, :)'; % uses logical indexing on CNTY_COVID to find the divisions.
     
     figure;
     tiledlayout(2, 1)
