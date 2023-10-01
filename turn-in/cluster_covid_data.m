1af9a751504820a5b19fd77f904da734bf38d979
ehitale <elatihe@gmail.com>
2023-10-01 13:11:31 -0500

    added training/testing data, renamed index variables

--- turn-in/cluster_covid_data.m
+++ turn-in/cluster_covid_data.m
@@ -2,11 +2,25 @@ load ("../COVIDbyCounty.mat")
 save ("cluster_covid_data.mat")
 
 k = 9; % for 9 divisions
-[idx, C] = kmeans(CNTY_COVID, k, "Replicates", 5); % this finds the kmeans  CNTY_COVID. I needed to transpose CNTY_COVID because the variables in CNTY_COVID are rows, whereas kmeans considers columns to be variables. Replication makes the kmeans look a whole lot better. 
+
+random = randperm(225);
+random_index = zeros(225, 1);
+
+for a = 1:length(random_index)
+    if random(a) <= 180
+        random_index(a) = 1;
+    end
+end
+
+trainingCNTY_COVID = CNTY_COVID(random_index == 1, :);
+testingCNTY_COVID = CNTY_COVID(random_index == 0, :);
+
+
+[k_idx, C] = kmeans(trainingCNTY_COVID, k, "Replicates", 5); % this finds the kmeans  CNTY_COVID. I needed to transpose CNTY_COVID because the variables in CNTY_COVID are rows, whereas kmeans considers columns to be variables. Replication makes the kmeans look a whole lot better. 
 
 for i = 1:9
-    caseTrajectories = CNTY_COVID(idx == i, :)'; % uses logical indexing on CNTY_COVID to find the divisions.
-    
+    caseTrajectories = trainingCNTY_COVID(idx == i, :)'; % uses logical indexing on CNTY_COVID to find the divisions.
+
     figure;
     tiledlayout(2, 1)
 
@@ -21,6 +35,7 @@ for i = 1:9
     title(['Cluster ', num2str(i)]);
     xlabel("week");
     ylabel("cases per 100k");
+
 end
 
 
