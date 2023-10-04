9745c007f629aff04e8e5fdc72aa7cd685909a51
ehitale <elatihe@gmail.com>
2023-10-04 14:41:22 -0500

    switched to 50 clusters, working towards comparison

--- turn-in/classify_covid_data.m
+++ turn-in/classify_covid_data.m
@@ -1,60 +1,41 @@
 load ("../COVIDbyCounty.mat")
 load ("cluster_covid_data.mat")
 
-% testingCNTY_COVID = CNTY_COVID(random_index == 0, :);
+pairDist = zeros(45, k);
+sPD = zeros(45, k);
+sPDI = zeros(45, k);
 
-pairDist = zeros(45, 9);
-sPD = zeros(45, 9);
-sPDI = zeros(45, 9);
-
-actualTrainingDivisions = CNTY_CENSUS.DIVISION(labeledCNTY_COVID(:,157)==1);
-actualTestingDivisions = CNTY_CENSUS.DIVISION(labeledCNTY_COVID(:,157)==0);
+% actualTrainingDivisions = CNTY_CENSUS.DIVISION(random_index==1);
+% actualTestingDivisions = CNTY_CENSUS.DIVISION(random_index==0);
 trainingCNTY_CENSUS = CNTY_CENSUS(random_index==1, :);
+testingCNTY_CENSUS = CNTY_CENSUS(random_index==0, :);
 
 divisionNames = unique(sortrows(CNTY_CENSUS, "DIVISION").DIVNAME, "stable");
-divisionMode = zeros(9, 1);
-clusterLabels = strings(9, 1);
+divisionMode = zeros(50, 1);
+clusterLabels = strings(50, 1);
 
-validModes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+% I have the divisions of the training data, from 1 to 9. I have the
+% cluster assignments for the training data, from 1 to 50. I need to assign
+% meanings to those 50 cluster assignments, and then go from those meaning
+% to divisions. 
 
-for i = 1:9
-    clusterDivisions = trainingCNTY_CENSUS.DIVISION(k_idx == i);
-    divisionMode(i) = mode(clusterDivisions);
-
-    for j = 1:9
-        if i ~= j
-            while divisionMode(j) == divisionMode(i)
-                clusterDivisions = clusterDivisions(clusterDivisions ~= divisionMode(i));
-                divisionMode(i) = mode(clusterDivisions); %still bugs, but made lots of progress.
-            end
-        end
-    end
-end
-
-[memberArray, member_index] = ismember(validModes, divisionMode);
-divisionMode(isnan(divisionMode)) = validModes(member_index == 0);
-
-for i = 1:9
+for i = 1:50
+    % fprintf("Cluster %d\n", i);
+    divisionMode(i) = mode(trainingCNTY_CENSUS.DIVISION(k_idx == i));
     clusterLabels(i) = divisionNames(divisionMode(i));
-end
+end % this code gives me the division each cluster appears to match best. e.g. cluster 18 is full of counties from Florida, i.e. the South Atlantic division so it has the South Atlantic Division name. 
 
 for i = 1:size(testingCNTY_COVID, 1) % 1:45
-    % figure; 
     testCounty = testingCNTY_COVID(i, :);
 
-    for j = 1:size(C, 1) % 1:9
+    for j = 1:size(C, 1) % 1:50
         testCombo = cat(1, testCounty, C(j, :)); % some example county on top of a centroid ('s case trajectory)
-        pairDist(i, j) = pdist(testCombo); % finds the euclidean distance between the example county and the centoid.
+        pairDist(i, j) = pdist(testCombo); % finds the euclidean distance between the example county and the centoid. maybe change to norm(x1 -x2)?
 
         [sPD(i, :), sPDI(i, :)] = sort(pairDist(i, :)); % sorts the euclidean distances and returns them sorted by ascending and their respective indices.
     end 
 
 end
-% in this case, cluster 1 is division 2, cluster 2 is division 1, cluster 3 is division 7, so on and so forth.
-% figure(1);
-% nexttile;
-% 
-% plot(1:0.01:10);
 
 % for i = 1:size(testingCNTY_COVID, 1) % 1:45
 %     fprintf("%d is the closest cluster for county %d\n", sPDI(i, 1), i)
@@ -62,9 +43,3 @@ end
 % 
 %     % if the two are the same, yay! else, nay!
 % end
-
-% alpha = [1, 2, 3];
-% beta = [7, 2, 3];
-% alphabeta = cat(1, alpha, beta);
-% pdist(alphabeta); % this will return '6', because the euclidean distance
-% between alpha and beta is 6.
--- turn-in/cluster_covid_data.m
+++ turn-in/cluster_covid_data.m
@@ -1,5 +1,5 @@
 load ("../COVIDbyCounty.mat")
-k = 9; % for 9 divisions
+k = 50; % for  number of clusters
 
 random = randperm(225);
 random_index = zeros(225, 1);
@@ -17,23 +17,23 @@ testingCNTY_COVID = CNTY_COVID(random_index == 0, :);
 
 [k_idx, C] = kmeans(trainingCNTY_COVID, k, "Replicates", 5); % this finds the kmeans  CNTY_COVID. Replication makes the kmeans look a whole lot better. 
 
-for i = 1:9
+for i = 1:k
     caseTrajectories = trainingCNTY_COVID(k_idx == i, :)'; % uses logical indexing on CNTY_COVID to find the clusters.
 
-    figure;
-    tiledlayout(3, 1)
-
-    nexttile;
-    plot(C(i, :));
-    title(['Centroid ', num2str(i)]);
-    xlabel("week");
-    ylabel("cases per 100k");
-
-    nexttile;
-    plot(caseTrajectories);
-    title(['Cluster ', num2str(i)]);
-    xlabel("week");
-    ylabel("cases per 100k");
+    % figure;
+    % tiledlayout(2, 1)
+    % 
+    % nexttile;
+    % plot(C(i, :));
+    % title(['Centroid ', num2str(i)]);
+    % xlabel("week");
+    % ylabel("cases per 100k");
+    % 
+    % nexttile;
+    % plot(caseTrajectories);
+    % title(['Cluster ', num2str(i)]);
+    % xlabel("week");
+    % ylabel("cases per 100k");
 
 end
 save ("cluster_covid_data.mat")
